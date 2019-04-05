849f44488e155891433457d5179e9a6179c212f7
Siva Gopi <siva.gopi@identos.ca>
2019-04-05 13:31:47 -0400

    initial commit

---
+++ guest-services/src/main/java/com/siva/security/services/GuestRepository.java
@@ -0,0 +1,19 @@
+package com.siva.security.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface GuestRepository extends JpaRepository<Guest, Long> {
+}
