e12ed1905e499006256eaa024f607c26a78a73a7
Siva Gopi <siva.gopi@identos.ca>
2019-04-05 15:02:26 -0400

    JDBC authentication with Bcrypt

--- guest-app/src/main/java/com/siva/ApplicationSecurityConfiguration.java
+++ guest-app/src/main/java/com/siva/ApplicationSecurityConfiguration.java
@@ -1,13 +1,20 @@
 package com.siva;
 
+import com.siva.security.app.auth.SivaUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.util.ArrayList;
@@ -16,6 +23,22 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private SivaUserDetailsService sivaUserDetailsService;
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider (){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(sivaUserDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(11));
+        return provider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -28,14 +51,5 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
                 .httpBasic();
     }
 
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
 
-        List<UserDetails> users = new ArrayList<>();
-        users.add(User.withDefaultPasswordEncoder().username("siva").password("password").roles("USER","ADMIN").build());
-        users.add(User.withDefaultPasswordEncoder().username("arun").password("foobar").roles("USER").build());
-        return new InMemoryUserDetailsManager(users);
-
-    }
 }
